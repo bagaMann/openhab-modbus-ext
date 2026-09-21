80a58b7958b0f1c909fc4c4e59e72270a847a396
bagaMann <117680707+bagaMann@users.noreply.github.com>
2026-09-21 13:11:58 +0300

    feat: add initial binding metadata and configs

---
+++ src/main/java/org/openhab/binding/modbusext/internal/config/ModbusChannelConfig.java
@@ -0,0 +1,33 @@
+package org.openhab.binding.modbusext.internal.config;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+@NonNullByDefault
+public class ModbusChannelConfig {
+    public String readStart = "";
+    public String readValueType = "uint16";
+    public String readTransform = "default";
+
+    public String writeStart = "";
+    public String writeValueType = "uint16";
+    public String writeType = "holding";
+    public String writeTransform = "default";
+
+    public long updateUnchangedValuesEveryMillis = 1000;
+
+    public boolean hasReadAddress() {
+        return !readStart.isBlank();
+    }
+
+    public boolean hasWriteAddress() {
+        return !writeStart.isBlank();
+    }
+
+    @Override
+    public String toString() {
+        return "ModbusChannelConfig{readStart='" + readStart + "', readValueType='" + readValueType
+                + "', writeStart='" + writeStart + "', writeValueType='" + writeValueType + "', writeType='"
+                + writeType + "'}";
+    }
+}

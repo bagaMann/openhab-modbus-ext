fcee0384ce22a303123e4d029c0feb15e82f67f5
bagaMann <117680707+bagaMann@users.noreply.github.com>
2026-09-22 07:11:36 +0300

    refactor: define four specialized poller thing types

--- src/main/java/org/openhab/binding/modbusext/internal/ModbusExtBindingConstants.java
+++ src/main/java/org/openhab/binding/modbusext/internal/ModbusExtBindingConstants.java
@@ -11,10 +11,16 @@ public final class ModbusExtBindingConstants {
 
     public static final ThingTypeUID THING_TYPE_TCP = new ThingTypeUID(BINDING_ID, "tcp");
     public static final ThingTypeUID THING_TYPE_SERIAL = new ThingTypeUID(BINDING_ID, "serial");
-    public static final ThingTypeUID THING_TYPE_POLLER = new ThingTypeUID(BINDING_ID, "poller");
+    public static final ThingTypeUID THING_TYPE_COIL_POLLER = new ThingTypeUID(BINDING_ID, "coil-poller");
+    public static final ThingTypeUID THING_TYPE_DISCRETE_POLLER = new ThingTypeUID(BINDING_ID, "discrete-poller");
+    public static final ThingTypeUID THING_TYPE_HOLDING_POLLER = new ThingTypeUID(BINDING_ID, "holding-poller");
+    public static final ThingTypeUID THING_TYPE_INPUT_POLLER = new ThingTypeUID(BINDING_ID, "input-poller");
+
+    public static final Set<ThingTypeUID> POLLER_THING_TYPES = Set.of(THING_TYPE_COIL_POLLER,
+            THING_TYPE_DISCRETE_POLLER, THING_TYPE_HOLDING_POLLER, THING_TYPE_INPUT_POLLER);
 
     public static final Set<ThingTypeUID> SUPPORTED_THING_TYPES = Set.of(THING_TYPE_TCP, THING_TYPE_SERIAL,
-            THING_TYPE_POLLER);
+            THING_TYPE_COIL_POLLER, THING_TYPE_DISCRETE_POLLER, THING_TYPE_HOLDING_POLLER, THING_TYPE_INPUT_POLLER);
 
     private ModbusExtBindingConstants() {
     }
